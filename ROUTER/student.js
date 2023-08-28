9cd495a13d8427d75f2de08a9d62771eb400321f
sudhir04121995 <mechsudharsanan555@gmail.com>
2023-08-28 16:58:01 +0530

    complete application development

--- ROUTER/student.js
+++ ROUTER/student.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllStudents } from "../collections/student.js";
+import { addStudents, deleteStudentById, editStudentById, getAllStudents, getElementById } from "../collections/student.js";
 
 
 //initializing router
@@ -18,5 +18,71 @@ router.get("/all",async(req,res)=>{
    res.status(500).send({message:"internal server error"})
     }
 })
+
+//get element by id
+router.get("/:id",async(req,res)=>{
+    try{
+      const {id} =req.params;
+      const student = await getElementById(id);
+      if(!student){
+        return res.status(400).send({message:"No Content Available"})
+      }
+      return res.status(200).send(student)
+    }catch(error){
+        console.log(error)
+        res.status(500).send({message:"internal server error"})
+    }
+})
+
+
+// add new student information
+router.post("/add",  async(req,res)=>{
+    try{
+     if(Object.keys(req.body).length <=0){
+        return res.status(400).send({message:"No content available"})
+     }
+     const newStudent = await addStudents(req.body);
+     if(!newStudent.acknowledged){
+        return res.status(400).send({message:"Cannot add data"})
+     }
+     res.status(201).send({result:newStudent,data:req.body})
+    }catch(error){
+console.log(error)
+res.status(500).send({message:"internal server error"});
+ }
+});
+
+// put
+
+router.put("/edit/:id",async(req,res)=>{
+    try{
+    const {id} = req.params;
+    if(!id ||Object.keys(req.body).length<=0){
+        return res.status(400).send({message:"Not A valid Request"})
+    }
+    const editedResult = await editStudentById(id ,req.body);
+    res.status(200).send({result:editedResult, data:req.body})
+    }catch(error){
+        console.log(error)
+        res.status(500).send({message:"internal server error"});
+    }
+})
+
+//Delete
+router.delete("/delete/:id",async(req,res)=>{
+    try{
+    const {id} = req.params;
+    if(!id){
+        return res.status(400).send({message:"Not A Valid Request"})
+    }
+    const deletedRequest = await deleteStudentById(id);
+    res.status(200).send({result:deletedRequest, success:"deleted successfully"})
+    }catch(error){
+        console.log(error)
+        res.status(500).send({message:"internal server error"});
+
+    }
+})
+
 
 export const studentRouter = router;
--- collections/student.js
+++ collections/student.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { client } from "../db.js"
 
 export function getAllStudents(req){
@@ -8,3 +9,31 @@ export function getAllStudents(req){
     .toArray()
   
 } 
+
+export function getElementById(id){
+    return client
+    .db("bootcamp")
+    .collection("StudentList")
+    .findOne({_id: new ObjectId(id)})
+}
+
+export function addStudents(data){
+    return client
+    .db("bootcamp")
+    .collection("StudentList")
+    .insertOne(data);
+}
+
+export function editStudentById(id,data){
+    return client
+    .db("bootcamp")
+    .collection("StudentList")
+    .findOneAndUpdate({_id: new ObjectId(id)},{$set:data})
+}
+
+export function deleteStudentById(id){
+    return client
+    .db("bootcamp")
+    .collection("StudentList")
+    .findOneAndDelete({_id:new ObjectId(id)})
+}
